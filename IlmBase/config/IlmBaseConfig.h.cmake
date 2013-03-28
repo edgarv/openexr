0b5725227ae4f1749e81422278a15c233eb5786d
Edgar Velazquez-Armendariz <eva5@cs.cornell.edu>
2013-03-28 16:43:35 -0400

    Initial CMake fixes for IlmBase.

--- IlmBase/config/IlmBaseConfig.h.cmake
+++ IlmBase/config/IlmBaseConfig.h.cmake
@@ -13,7 +13,15 @@
 
 #cmakedefine HAVE_POSIX_SEMAPHORES 1
 
+
 #cmakedefine HAVE_UCONTEXT_H 1
+
+
+//
+// Dealing with FPEs
+//
+#cmakedefine ILMBASE_HAVE_CONTROL_REGISTER_SUPPORT 1
+
 
 //
 // Define and set to 1 if the target system has support for large
@@ -24,7 +32,31 @@
 
 
 //
+// Current (internal) library namepace name and corresponding public
+// client namespaces.
+//
+#cmakedefine ILMBASE_INTERNAL_NAMESPACE_CUSTOM @ILMBASE_INTERNAL_NAMESPACE_CUSTOM@
+#cmakedefine IMATH_INTERNAL_NAMESPACE @IMATH_INTERNAL_NAMESPACE@
+#cmakedefine IEX_INTERNAL_NAMESPACE @IEX_INTERNAL_NAMESPACE@
+#cmakedefine ILMTHREAD_INTERNAL_NAMESPACE @ILMTHREAD_INTERNAL_NAMESPACE@
+
+#cmakedefine ILMBASE_NAMESPACE_CUSTOM @ILMBASE_NAMESPACE_CUSTOM@
+#cmakedefine IMATH_NAMESPACE @IMATH_NAMESPACE@
+#cmakedefine IEX_NAMESPACE @IEX_NAMESPACE@
+#cmakedefine ILMTHREAD_NAMESPACE @ILMTHREAD_NAMESPACE@
+
+
+//
 // Version string for runtime access
 //
-#cmakedefine ILMBASE_VERSION_STRING @ILMBASE_VERSION_STRING@
-#cmakedefine ILMBASE_PACKAGE_STRING @ILMBASE_PACKAGE_STRING@
+#define ILMBASE_VERSION_STRING @ILMBASE_VERSION_STRING@
+#define ILMBASE_PACKAGE_STRING @ILMBASE_PACKAGE_STRING@
+
+#define ILMBASE_VERSION_MAJOR @ILMBASE_VERSION_MAJOR@
+#define ILMBASE_VERSION_MINOR @ILMBASE_VERSION_MINOR@
+#define ILMBASE_VERSION_PATCH @ILMBASE_VERSION_PATCH@
+
+// Version as a single hex number, e.g. 0x01000300 == 1.0.3
+#define ILMBASE_VERSION_HEX ((ILMBASE_VERSION_MAJOR << 24) | \
+                             (ILMBASE_VERSION_MINOR << 16) | \
+                             (ILMBASE_VERSION_PATCH <<  8))
